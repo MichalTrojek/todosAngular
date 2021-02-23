df658cd0bf18dca7720af22b6155053a6973d52e
MichaTrojek <MichalTrojek1@gmail.com>
2021-02-23 22:37:41 +0100

    first init

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoServiceService } from '../../services/todo-service.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css'],
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor(private todoService: TodoServiceService) {}
+
+  ngOnInit(): void {}
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed,
+    };
+    return classes;
+  }
+
+  onToggle(todo: Todo) {
+    //Toggle in UI
+    todo.completed = !todo.completed;
+    //Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe((todo) => {
+      console.log(todo);
+    });
+  }
+
+  onDelete(todo: Todo) {
+    console.log('delete');
+  }
+}
